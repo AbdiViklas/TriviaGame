dd6f6e2118749c5633c0828b091a84a93b7049fa
Andy Bonner <abonner@duke.edu>
2017-06-14 10:55:22 -0400

    footer w link to HopeMed

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
 /*
-Trivia topics/themes:
-- animals (kid friendly)
-- music (too obvious / type-casting / niche geeky?)
-- another media fan franchise
-- Sierra Leone
+1. User loads page. Introductory modal/screen displays, with prep/instructions. "When you're ready, push START"
+2. User pushes start, launching first page of quiz.
+  2a. Timer starts and displays; properties of first question-object display
+  NB: maybe question-objects should be stored in a big array this time instead of an object, to access them sequentially
+3. EITHER...
+  3a. User chooses the right answer, in which case...
+    4a. Display success message and pic associated with that question, OR
+  3b. User chooses a wrong answer, in which case...
+    4b. Display same page, except with "Wrong!" message instead of "Right!"
+  3c. User makes no choice and timer runs out, in which case ...
+    4c. Display same page, with "Time's Up!"
 */
