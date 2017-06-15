56fc61594412010bdc856ef41db071e3e0fb37ca
Andy Bonner <abonner@duke.edu>
2017-06-15 18:03:05 -0400

    begin building runQuestion()

--- assets/script.js
+++ assets/script.js
@@ -16,9 +16,7 @@ var questionArray = [
   {
     question: "Sierra Leone is located...",
     correctAnswer: "in western Africa, between Guinea and Liberia",
-    wrongAnswer1: "in southern Africa, between Namibia and Zimbabwe",
-    wrongAnswer2: "in eastern Africa, between Somalia and Sudan",
-    wrongAnswer3: "just north of Greenland",
+    wrongAnswers: ["in southern Africa, between Namibia and Zimbabwe", "in eastern Africa, between Somalia and Sudan", "just north of Greenland"],
     doneMessage: "Sierra Leone is a coastal counry in West Africa, with Liberia on its southeast and Guinea wrapping around its northern border.",
     image: "<img src='assets\images\west_africa_map.png' alt='map of West Africa'>",
     imageCaption: "Map of West Africa"
@@ -26,9 +24,7 @@ var questionArray = [
   {
     question: "Sierra Leone was, for over a century, a colony of...",
     correctAnswer: "England",
-    wrongAnswer1: "Spain",
-    wrongAnswer2: "Germany",
-    wrongAnswer3: "the United States",
+    wrongAnswers: ["Spain", "Germany", "the United States"],
     doneMessage: "Although the Portuguese, French, and Dutch were represented in the area in the 15th and 16th centuries, Sierra Leone became a British colony in 1787 and achieved full independence in 1960.",
     image: "<img src='assets\images\Flag_of_Sierra_Leone_1916-1961.gif' alt='flag of Sierra Leone, 1916-1961'>",
     imageCaption: "Flag of Sierra Leone, 1916-1961"
@@ -36,17 +32,28 @@ var questionArray = [
   {
     question: "In the 18th century Sierra Leone was...",
     correctAnswer: "a major point of deportation in the slave trade",
-    wrongAnswer1: "a major exporter of tropical fruit",
-    wrongAnswer2: "a major producer of sugar",
-    wrongAnswer3: "a major source of iron ore",
+    wrongAnswers: ["a major exporter of tropical fruit", "a major producer of sugar", "a major source of iron ore"],
     doneMessage: "At the height of the slave trade, tens of thousands of slaves were funneled through Sierra Leone's ports. Rice cultivation was central to the region, and the rice plantations of the American southern Lowlands placed a high premium on slaves with this knowledge.",
     image: "<img src='bunce.jpg' alt='18th-century woodcut of Bunce Island'>",
     imageCaption: "<a href='https://en.wikipedia.org/wiki/Bunce_Island' target='_blank'>Bunce Island</a>, a slave &ldquo;factory&rdquo; where thousands of slaves were processed before deportation directly to South Carolina and Georgia"
   }
 ];
 
+var seconds = 20;
+
+var questionOrder = [0, 1, 2, 3];
+
 function runQuestion(object) {
-  
+  $("#seconds").html(seconds);
+  // TODO: create timer function and call to update
+  $("#question").html(object.question);
+  questionOrder.sort(function(a, b){return 0.5 - Math.random()}); // randomize question order
+  $("#answer" + questionOrder[0]).html(object.correctAnswer).addClass("correct");
+  // correct answer is sent to the first randomized index number
+  for (var i = 1; i < questionOrder.length; i++) {
+    var questionIndex = questionOrder[i]; //starting with the second element in questionOrder...
+    $("#answer" + questionIndex).html(object.wrongAnswers[i - 1]);
+  }
 }
 
 function displayResults() {
@@ -60,8 +67,23 @@ function runQuiz() {
   displayResults();
 }
 
-$("#startBtn").on(click, function () {
-  $("#main-card-content").html(`
-    
+$("#startBtn").on("click", function () {
+  $("#main-card").html(`
+    <div id="main-card-content" class="card-content">
+      <div id="timer" class="right">
+        <i class="material-icons">hourglass_empty</i>
+        Time remaining: 0:<span id="seconds"></span>
+      </div>
+      <h2 id="question"></h2>
+      <div class="card-action">
+        <div class="container center-align">
+            <a id="answer0" class="waves-effect waves-light btn answer"></a>
+            <a id="answer1" class="waves-effect waves-light btn answer"></a>
+            <a id="answer2" class="waves-effect waves-light btn answer"></a>
+            <a id="answer3" class="waves-effect waves-light btn answer"></a>
+        </div>
+      </div>
+    </div>
   `);
+  runQuestion(questionArray[0]); //replace with some kind of loop eventually
 });
