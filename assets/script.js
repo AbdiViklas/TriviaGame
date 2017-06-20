8837bce446c8b412ae708b95dcd65323818e38a6
Andy Bonner <abonner@duke.edu>
2017-06-19 22:53:52 -0400

    add media query to alter layout of img on smaller screens

--- assets/script.js
+++ assets/script.js
@@ -23,7 +23,7 @@ var questionArray = [
     correctAnswer: "in western Africa, between Guinea and Liberia",
     wrongAnswers: ["in southern Africa, between Namibia and Zimbabwe", "in eastern Africa, between Somalia and Sudan", "just north of Greenland"],
     doneMessage: "Sierra Leone is a coastal counry in West Africa, with Liberia on its southeast and Guinea wrapping around its northern border.",
-    image: "<img src='assets\\images\\west_africa_map.png' class='responsive-img answer-img' alt='map of West Africa'>", // double \\ to escape the \ and make it render within the string literal of displayAnswer()
+    image: "<img src='assets\\images\\west_africa_map.png' class='responsive-img col s12 l10 offset-l1' alt='map of West Africa'>", // double \\ to escape the \ and make it render within the string literal of displayAnswer()
     imageCaption: "Map of West Africa"
   },
   {
@@ -31,7 +31,7 @@ var questionArray = [
     correctAnswer: "England",
     wrongAnswers: ["Spain", "Germany", "the United States"],
     doneMessage: "Although the Portuguese, French, and Dutch were represented in the area in the 15th and 16th centuries, Sierra Leone became a British colony in 1787 and achieved full independence in 1960.",
-    image: "<img src='assets\\images\\Flag_of_Sierra_Leone_1916-1961.gif' class='responsive-img answer-img' alt='flag of Sierra Leone, 1916-1961'>",
+    image: "<img src='assets\\images\\Flag_of_Sierra_Leone_1916-1961.gif' class='responsive-img col s12 l10 offset-l1' alt='flag of Sierra Leone, 1916-1961'>",
     imageCaption: "Flag of Sierra Leone, 1916-1961"
   },
   {
@@ -39,7 +39,7 @@ var questionArray = [
     correctAnswer: "a major point of deportation in the slave trade",
     wrongAnswers: ["a major exporter of tropical fruit", "a major producer of sugar", "a major source of iron ore"],
     doneMessage: "At the height of the slave trade, tens of thousands of slaves were funneled through Sierra Leone's ports. Rice cultivation was central to the region, and the rice plantations of the American southern Lowlands placed a high premium on slaves with this knowledge.",
-    image: "<img src='assets\\images\\bunce.jpg' class='responsive-img answer-img' alt='18th-century woodcut of Bunce Island'>",
+    image: "<img src='assets\\images\\bunce.jpg' class='responsive-img col s12 l10 offset-l1' alt='18th-century woodcut of Bunce Island'>",
     imageCaption: "<a href='https://en.wikipedia.org/wiki/Bunce_Island' target='_blank'>Bunce Island</a>, a slave &ldquo;factory&rdquo; where thousands of slaves were processed before deportation directly to South Carolina and Georgia"
   },
   {
@@ -47,7 +47,7 @@ var questionArray = [
     correctAnswer: "the relocation of thousands of freed slaves",
     wrongAnswers: ["mass immigration from Eastern Europe", "sweeping cholera epidemics", "mass emigration to Mali"],
     doneMessage: "Sierra Leone was targeted as a destination for resettlement, starting with thousands of <a href='https://en.wikipedia.org/wiki/Black_Loyalist' target='_blank'>Black Loyalists</a> who had fought for Britain in the American Revolution. After Britain outlawed slavery in 1807, Sierra Leone was also a base for naval forces patrolling to intercept illegal slave ships, and slaves liberated by these forces were returned to Sierra Leone (though often to be sold into indentured apprenticeship, or conscripted into the navy).",
-    image: "<img src='assets\\images\\cotton-tree.jpg' class='responsive-img answer-img' alt='Cotton Tree in Freetown'>",
+    image: "<img src='assets\\images\\cotton-tree.jpg' class='responsive-img col s12 l10 offset-l1' alt='Cotton Tree in Freetown'>",
     imageCaption: "The iconic <a href='https://en.wikipedia.org/wiki/Cotton_Tree_(Sierra_Leone)' target='_blank'>Cotton Tree</a> around which settlers built Freetown"
   },
   {
@@ -55,7 +55,7 @@ var questionArray = [
     correctAnswer: "a brutal civil war",
     wrongAnswers: ["a series of earthquakes", "severe drought", "international tarriffs"],
     doneMessage: "The 11 year civil war was marked by widespread atrocities against human rights, and left 50,000 people dead and the public infrastructure gutted. 1,270 schools were destroyed, many hospitals were destroyed or looted, and many healthcare professionals fled the country.",
-    image: "<img src='assets\\images\\school.jpg' class='responsive-img answer-img' alt='A school near Koindu, damaged in the civil war'>",
+    image: "<img src='assets\\images\\school.jpg' class='responsive-img col s12 l10 offset-l1' alt='A school near Koindu, damaged in the civil war'>",
     imageCaption: "A school near Koindu, damaged in the civil war"
   },
   {
@@ -63,7 +63,7 @@ var questionArray = [
     correctAnswer: "agriculture",
     wrongAnswers: ["diamond production", "tourism", "fishing"],
     doneMessage: "Agriculture has been an important part of West Africa for centuries. Today it represents 58% of Sierra Leone's GDP, and 80% of the population are involved in it. Two thirds of the population are involved in subsistence farming.<br>Nevertheless, Sierra Leone is a net importer of food, and although the most common crop is rice, it still imports rice.",
-    image: "<img src='assets\\images\\rice_farmer.jpg' class='responsive-img answer-img' alt='A Sierra Leonean rice farmer'>",
+    image: "<img src='assets\\images\\rice_farmer.jpg' class='responsive-img col s12 l10 offset-l1' alt='A Sierra Leonean rice farmer'>",
     imageCaption: "A Sierra Leonean rice farmer"
   },
   {
@@ -71,7 +71,7 @@ var questionArray = [
     correctAnswer: "Krio",
     wrongAnswers: ["French", "Swahili", "Mandinka"],
     doneMessage: "Krio is both <a href='https://en.wikipedia.org/wiki/Krio_language' target='_blank'>a language</a> and <a href='https://en.wikipedia.org/wiki/Sierra_Leone_Creole_people' target='_blank'>an ethnic group</a>, a synthesis of cultures brought from the American South, surrounding West African peoples, and especially Jamaica. Although the Krio descendants of freed slaves represent only 1% of the national population, the language unites Sierra Leone's other ethnic groups, primarily the <a href='https://en.wikipedia.org/wiki/Temne_people' target='_blank'>Temne</a> and the <a href='https://en.wikipedia.org/wiki/Mende_people' target='_blank'>Mende</a>.",
-    image: "<img src='assets\\images\\krio-gullah.jpg' class='responsive-img answer-img' alt='Comparison of Krio and Gullah'>",
+    image: "<img src='assets\\images\\krio-gullah.jpg' class='responsive-img col s12 l10 offset-l1' alt='Comparison of Krio and Gullah'>",
     imageCaption: "Comparison of Krio and <a href='https://en.wikipedia.org/wiki/Gullah' target='_blank'>Gullah</a>"
   },
   {
@@ -79,7 +79,7 @@ var questionArray = [
     correctAnswer: "Blood Diamond",
     wrongAnswers: ["Hotel Rwanda", "Invictus", "I Dreamed of Africa"],
     doneMessage: "<span class='italic'>Blood Diamond</span> tells a fictional story of diamond smuggling in the midst of Sierra Leone's civil war. Although set in Sierra Leone, <a href='https://mightyminnow.wordpress.com/2007/05/30/blood-diamond-should-have-been-shot-in-sierra-leone/' target='_blank'>the movie was filmed mostly in</a> South Africa and Mozambique.",
-    image: "<img src='assets\\images\\blood-diamond.jpg' class='responsive-img answer-img' alt='still from the movie Blood Diamond'>",
+    image: "<img src='assets\\images\\blood-diamond.jpg' class='responsive-img col s12 l10 offset-l1' alt='still from the movie Blood Diamond'>",
     imageCaption: "Still from the movie <span class='italic'>Blood Diamond</span>"
   },
   {
@@ -87,7 +87,7 @@ var questionArray = [
     correctAnswer: "cheetah",
     wrongAnswers: ["chimpanzee", "pygmy hippopotamus", "African manatee"],
     doneMessage: "Sierra Leone has a <a href='https://en.wikipedia.org/wiki/Wildlife_of_Sierra_Leone' target='_blank'>diverse wildlife population</a>, since it encompasses many different biomes--coastal beach, mangrove swamps, and inland rainforest. The endangered pygmy hippopotamus can be found in the Gola region. No cheetahs, though.",
-    image: "<img src='assets\\images\\pygmy-hippo.jpg' class='responsive-img answer-img' alt='A pygmy hippopotamus'>",
+    image: "<img src='assets\\images\\pygmy-hippo.jpg' class='responsive-img col s12 l10 offset-l1' alt='A pygmy hippopotamus'>",
     imageCaption: "A pygmy hippopotamus"
   }
 ];
@@ -146,7 +146,11 @@ function displayAnswer(outcomeMessage, object) {
     <div id="main-card-content" class="card-content">
       <span class="card-title flow-text extra-margin">${outcomeMessage}</span>
       <p class="extra-margin flow-text">${object.doneMessage}</p>
-      <div class="center-align extra-margin answer-image">${object.image}</div>
+      <div class="center-align extra-margin answer-image">
+        <div class="row">
+        ${object.image}
+        </div>
+      </div>
       <div class="center-align extra-margin">${object.imageCaption}</div>
       <div class="switch inline-block">
         Auto-advance:
